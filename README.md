47545defbbd7072000e5ce87d8cdb310f06e65b1
SahilK-027 <sahilkandhare07@gmail.com>
2024-08-25 18:19:10 +0530

    Update

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# React + Vite
+# R3F-template
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+To run the development server
+```bash
+npm install
+npm run dev
+```
 
-Currently, two official plugins are available:
+To run build production code 
+```bash
+npm run build
+```
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Renders A Rotating Cube
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
-import { OrbitControls, Shadow } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+function RotatingCube() {
+  const meshRef = useRef();
+
+  // Rotate the cube on every frame
+  useFrame(() => {
+    meshRef.current.rotation.x += 0.01;
+    meshRef.current.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry args={[1.25, 1.25, 1.25]} />
+      <meshNormalMaterial />
+    </mesh>
+  );
+}
 
 export default function App() {
   return (
     <>
       <Canvas>
-        <color attach="background" args={['#fff']} />
+        <color attach="background" args={["#121316"]} />
         <OrbitControls />
-        <mesh>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshNormalMaterial color="mediumpurple" />
-        </mesh>
+        <RotatingCube />
       </Canvas>
     </>
-  )
+  );
 }
